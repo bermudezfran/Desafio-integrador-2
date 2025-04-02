941365286189ca4ad1ebacffde287447b9d32ad9
Francisco Bermudez <bermudezfran15@gmail.com>
2025-04-02 14:35:21 -0300

    se dejaron los enlaces andando

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Link } from 'react-router-dom'
-import './App.css'
-import IndexRoute from './routes/Route'
+
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
 
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
-      <h1>Esta es la pagina de inicio</h1>
-      <Link to={'/nosotros'}>Nosotros</Link>
-      <Link to={'/contacto'}>Contacto</Link>
+      <h1 >Esta es la pagina de inicio</h1>
+      <Dashboard />
     </div>
   )
 }
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ContactoPage } from "../pages/ContactoPage";
 import { NosotrosPage } from "../pages/NosotrosPage";
+import { Dashboard } from "../pages/Dashboard";
 
 export default function IndexRoute() {
-
-
     return (
-        <Router>
-            <Route path="/contacto" element={<ContactoPage />} />
-            <Route path="/nosotros" element={<NosotrosPage />} />
-        </Router>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/contacto" element={<ContactoPage />} />
+                <Route path="/nosotros" element={<NosotrosPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
