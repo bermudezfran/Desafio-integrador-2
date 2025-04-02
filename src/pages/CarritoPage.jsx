72a2df10b5a500cd06e3a0f545439685746e2559
Francisco Bermudez <bermudezfran15@gmail.com>
2025-04-02 18:00:15 -0300

    se agrego boton de borrar

--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header";
 import { useCarrito } from "../context/CartStore";
 
 export const CarritoPage = () => {
-  const { carrito } = useCarrito();
+  const { carrito, eliminarDelCarrito } = useCarrito();
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   useEffect(() => {
     document.body.classList.toggle("dark-theme", theme === "dark");
@@ -14,25 +14,30 @@ export const CarritoPage = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
+  const handleDeleteItem = (id) => {
+    eliminarDelCarrito(id)
+  }
+
   return (
     <>
       <Header theme={theme} onToggleTheme={toggleTheme} />
       <div className="cart">
         {carrito.length > 0 ? (
           carrito.map((producto) => (
-            <div key={producto.id} className="cart-item">
+            <div key={producto.id} className="cartItem">
+              <img className="imgCart" src={producto.foto} alt={producto.nombre} />
+              <div className="info">
               <h2>{producto.nombre}</h2>
-              <img src={producto.foto} alt={producto.nombre} />
               <p>{producto.descripcion}</p>
-              <p>
-                <b>Precio:</b> {producto.precio}
-              </p>
+              <span className="price">Precio:{producto.precio}</span>
+              <button className="deleteItem" onClick={() => handleDeleteItem(producto.id)}>Eliminar</button>
+              </div>
             </div>
           ))
         ) : (
           <p>Tu carrito está vacío</p>
         )}
-      </div>
+        </div>
     </>
   );
 };
