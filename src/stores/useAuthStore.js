f9dd4d3a1e61bdc617283b3d53a8584772e0fa41
Francisco Bermudez <bermudezfran15@gmail.com>
2025-05-06 16:39:56 -0300

    feat: add auth context

---
+++ src/stores/useAuthStore.js
@@ -0,0 +1,23 @@
+import create from "zustand";
+import Cookies from "js-cookie";
+import api from "../services/api";
+
+export const useAuthStore = create((set) => ({
+  token: Cookies.get("token") || null,
+  user: null,
+  // Registrar para el user
+  register: async (email, password) => {
+    await api.post("/auth/register", { email, password });
+  },
+  // Login para el user
+  login: async (email, password) => {
+    const { data } = await api.post("/auth/login", { email, password });
+    Cookies.set("token", data.token);
+    set({ token: data.token, user: data.user });
+  },
+  // Cierre de sesion del user
+  logout: () => {
+    Cookies.remove("token");
+    set({ token: null, user: null });
+  },
+}));
