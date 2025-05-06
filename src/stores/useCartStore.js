85cd9678f945f87873a23fa7b112ee00f64ab375
Francisco Bermudez <bermudezfran15@gmail.com>
2025-05-06 16:40:42 -0300

    add cart store

---
+++ src/stores/useCartStore.js
@@ -0,0 +1,22 @@
+import create from 'zustand'
+
+export const useCartStore = create((set) => ({
+  items: [],
+  addItem: (product, qty = 1) =>
+    set((state) => {
+      const exists = state.items.find(i => i.product.id === product.id)
+      if (exists) {
+        return {
+          items: state.items.map(i =>
+            i.product.id === product.id
+              ? { ...i, qty: i.qty + qty }
+              : i
+          )
+        }
+      }
+      return { items: [...state.items, { product, qty }] }
+    }),
+  removeItem: (id) =>
+    set((state) => ({ items: state.items.filter(i => i.product.id !== id) })),
+  clearCart: () => set({ items: [] })
+}))
